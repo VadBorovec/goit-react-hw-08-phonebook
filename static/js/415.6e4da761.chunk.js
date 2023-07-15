c52dd5d861b0b469fe2c31c5f5d18469548bd8b9
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-07-15 20:24:34 +0000

    Deploying to gh-pages from @ VadBorovec/goit-react-hw-08-phonebook@b9fafc2c3f379a804cc07a8b914a3b3dfd3a6378 🚀

--- static/js/415.6e4da761.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,n,o){o.r(n),o.d(n,{default:function(){return h}});var t=o(4270),r=o(4091),c=o(3329);function h(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(t.q,{children:(0,c.jsx)("title",{children:"Phonebook - Home"})}),(0,c.jsx)(r.VM,{})]})}}}]);
-//# sourceMappingURL=415.6e4da761.chunk.js.map
